d3aee3905cb67cc7275fd46ebdd30c9be82c04ee
arthwr <zenarthwr@gmail.com>
2025-06-03 11:24:54 +0500

    feat(block type retrieval): add block_to_block_type method and related tests

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,41 @@
+import re
+
+from enum import Enum
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+
 def markdown_to_blocks(markdown):
     blocks = (block.strip() for block in markdown.split("\n\n"))
     return [block for block in blocks if block]
+
+
+def block_to_block_type(markdown_block):
+    lines = markdown_block.strip().split("\n")
+
+    if markdown_block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
+        return BlockType.HEADING
+
+    if len(lines) > 1 and lines[0].startswith("```") and lines[-1].startswith("```"):
+        return BlockType.CODE
+
+    if all(line.startswith(">") for line in lines if line.strip()):
+        return BlockType.QUOTE
+
+    if all(line.startswith("- ") for line in lines if line.strip()):
+        return BlockType.UNORDERED_LIST
+
+    for idx, line in enumerate(lines):
+        if not line.startswith(f"{idx+1}. "):
+            break
+    else:
+        return BlockType.ORDERED_LIST
+
+    return BlockType.PARAGRAPH
--- tests/test_block_markdown.py
+++ tests/test_block_markdown.py
@@ -1,5 +1,5 @@
 import unittest
-from src.block_markdown import markdown_to_blocks
+from src.block_markdown import BlockType, markdown_to_blocks, block_to_block_type
 
 
 class TestBlockMarkdownSplit(unittest.TestCase):
@@ -46,6 +46,98 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+
+
+class TestBlockToBlockType(unittest.TestCase):
+    def test_block_to_heading1(self):
+        text_block = "# This is a heading"
+        block_type = block_to_block_type(text_block)
+
+        self.assertEqual(block_type, BlockType.HEADING)
+
+    def test_block_to_heading2(self):
+        text_block = "## This is a heading"
+        block_type = block_to_block_type(text_block)
+
+        self.assertEqual(block_type, BlockType.HEADING)
+
+    def test_block_to_heading6(self):
+        text_block = "###### This is a heading"
+        block_type = block_to_block_type(text_block)
+
+        self.assertEqual(block_type, BlockType.HEADING)
+
+    def test_block_to_wrong_heading(self):
+        text_block = "####### This is a heading"
+        block_type = block_to_block_type(text_block)
+
+        self.assertNotEqual(block_type, BlockType.HEADING)
+
+    def test_block_to_wrong_nospace__heading(self):
+        text_block = "#This is a heading"
+        block_type = block_to_block_type(text_block)
+
+        self.assertNotEqual(block_type, BlockType.HEADING)
+
+    def test_block_to_code(self):
+        text_block = """```js
+console.log("Hello world")
+```"""
+        block_type = block_to_block_type(text_block)
+
+        self.assertEqual(block_type, BlockType.CODE)
+
+    def test_block_to_code_single(self):
+        text_block = """```
+code
+```"""
+        block_type = block_to_block_type(text_block)
+
+        self.assertEqual(block_type, BlockType.CODE)
+
+    def test_block_to_quote_multiple(self):
+        text_block = """> Hello
+> This is a quote
+> This one as well"""
+
+        block_type = block_to_block_type(text_block)
+        self.assertEqual(block_type, BlockType.QUOTE)
+
+    def test_block_to_unordered_list(self):
+        text_block = """- This is the first list item in a list block
+- This is a list item
+- This is another list item
+"""
+        block_type = block_to_block_type(text_block)
+
+        self.assertEqual(block_type, BlockType.UNORDERED_LIST)
+
+    def test_block_to_ordered_list(self):
+        text_block = """1. This is the first list item in a list block
+2. This is a list item
+3. This is another list item
+"""
+        block_type = block_to_block_type(text_block)
+
+        self.assertEqual(block_type, BlockType.ORDERED_LIST)
+
+    def test_block_to_invalid_starting_ordered_list(self):
+        text_block = """2. This is the first list item in a list block
+3. This is a list item
+4. This is another list item
+"""
+        block_type = block_to_block_type(text_block)
+
+        self.assertNotEqual(block_type, BlockType.ORDERED_LIST)
+
+    def test_block_to_misordered_list(self):
+        text_block = """1. This is the first list item in a list block
+3. This is a list item
+5. This is another list item
+"""
+        block_type = block_to_block_type(text_block)
+
+        self.assertNotEqual(block_type, BlockType.ORDERED_LIST)
 
 
 if __name__ == "__main__":
